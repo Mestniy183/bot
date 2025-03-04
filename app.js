26e5ee46f06cb223583539262225173cd4c3a683
Mestniy183 <ken-zero@ya.ru>
2025-03-04 08:12:48 +0300

    done

--- app.js
+++ app.js
@@ -1,43 +1,107 @@
-import TelegramBot from 'node-telegram-bot-api';
-import { get } from 'axios';
+const TelegramBot = require("node-telegram-bot-api");
+const axios = require("axios");
+const fs = require("fs");
+const path = require("path");
+require("dotenv").config();
 
-const token = '7341329435:AAG2EgMctrPqFUN3vYgz6RpkwdCdG7KUBQw';
-const bot = new TelegramBot(token, { poling: true });
-const apiUrl = 'https://dc3c997554c1d330.mokky.dev/items';
+const token = process.env.TOKEN;
+const bot = new TelegramBot(token, { polling: true });
+const apiUrl = process.env.API_URL;
 
-async function fetchData() {
-    try {
-        const response = await get(apiUrl);
-        return response.data;
-    } catch (arror) {
-        console.error('Ошибка при получении данных:', error);
-        return null;
+bot.setMyCommands([
+  { command: "/start", description: "Запуск бота и получение данных" },
+  { command: "/orders", description: "Посмотреть все заказы" },
+]);
+
+const dataFilePath = path.join(__dirname, "db.json");
+let lastData = [];
+try {
+  if (fs.existsSync(dataFilePath)) {
+    const data = fs.readFileSync(dataFilePath, "utf8");
+    if (data.trim()) {
+      lastData = JSON.parse(data);
+    } else {
+      console.log("Файл db.json пустой. Инициализируем пустым массивом");
+      lastData = [];
     }
+  } else {
+    console.log("Файл db.json не существует. Инициализируем пустым массивом");
+    lastData = [];
+  }
+} catch (error) {
+  console.error("Ошибка при чтении файла db.json:", error);
+  lastData = [];
 }
 
-function sendMessage(chatId, message) {
-    bot.sendMessage(chatId, message);
+function saveData(data) {
+  try {
+    fs.writeFileSync(dataFilePath, JSON.stringify(data, null, 2), "utf8");
+  } catch (error) {
+    console.error("Ошибка при сохранении db.JSON", error);
+  }
+}
+
+async function fetchData() {
+  try {
+    const response = await axios.get(apiUrl);
+    return response.data;
+  } catch (arror) {
+    console.error("Ошибка при получении данных:", error);
+    return null;
+  }
 }
 
 async function checkForNewData(chatId) {
+  try {
     const data = await fetchData();
     if (data && data.length > 0) {
-        data.forEach(element => {
-            const message = `Новый элемент: ${JSON.stringify(element)}`;
-            sendMessage(chatId, message);
+      const newData = data.filter((item) => {
+        return !lastData.some((lastItem) => lastItem.id === item.id);
+      });
+
+      if (newData.length > 0) {
+        newData.forEach((item) => {
+          const mes = `Пришёл новый заказ ${item.id}:\nДата: ${item.date}\nИмя: ${item.name}\nТелефон: ${item.phone}\nСообщение: ${item.message}`;
+          bot.sendMessage(chatId, mes);
         });
+        lastData = data;
+        saveData(lastData);
+      }
     } else {
-        sendMessage(chatId, `Новых данных нет`)
+      console.log("Данные сервера отсутствуют или пустые");
     }
+  } catch (error) {
+    console.error("Ошибка при проверке новых данных", error);
+    bot.sendMessage(chatId, "Ошибка при проверке новых данных");
+  }
 }
 
-bot.onText(/\start/, (msg) =>{
-    const chatId = msg.chat.id;
+setInterval(() => {
+  checkForNewData(process.env.ID);
+}, 10000);
+
+bot.on("message", async (msg) => {
+  const chatId = msg.chat.id;
+  const text = msg.text;
+
+  if (text === "/start") {
     bot.sendMessage(chatId, `Бот запущен, проверяем данные...`);
-    setInterval(() =>{
-        checkForNewData(chatId)
-    }, 10000)
-})
-bot.on('polling_error', (error) =>{
-    console.error(error);
-})
+    checkForNewData(chatId);
+  } else if (text === "/orders") {
+    bot.sendMessage(chatId, `Отправляю все заказы...`);
+    const data = await fetchData();
+    console.log(data);
+    data.forEach((item, index) => {
+      setTimeout(() => {
+        const mes = `Заказ ${item.id}:\nДата: ${item.date}\nИмя: ${item.name}\nТелефон: ${item.phone}\nСообщение: ${item.message}`;
+        bot.sendMessage(chatId, mes);
+      }, index * 100);
+    });
+  } else {
+    bot.sendMessage(chatId, `Неизвестная команда...`);
+  }
+});
+
+bot.on("polling_error", (error) => {
+  console.error(error);
+});
