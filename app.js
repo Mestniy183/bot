f541d4f966241c3aeca6a8f3032f40e7d4bd015e
Mestniy183 <ken-zero@ya.ru>
2025-05-18 11:21:18 +0300

    d

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ require("dotenv").config();
 const token = process.env.TOKEN;
 const apiUrl = process.env.API_URL;
 const adminChatId = process.env.ID;
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 const webhookURL = process.env.WEBHOOK_URL;
 
 const bot = new TelegramBot(token, { webHook: { port: port } });
